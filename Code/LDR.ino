fceac002934a3ff95707bfec926f70648edc74e2
Maximilien-Kulbicki <95374519+Maximilien-Kulbicki@users.noreply.github.com>
2022-01-05 12:35:44 +0100

    Add files via upload

--- Code/LDR.ino
+++ Code/LDR.ino
@@ -1,19 +1,27 @@
 #define LDR A0 // composante photorésistance sur la pin A0
 
-int value;
+int lightValue;
  
 void setup() {
-   // initialise la communication avec le PC
    Serial.begin(9600);
-
-   // initialise les broches
    pinMode(LDR, INPUT);
 }
- 
-void loop() {
-   // mesure la tension sur la broche A0
-   value = analogRead(LDR);
-   Serial.println(value);
 
-   delay(100);
+void loop() {
+   lightValue = analogRead(LDR);
+   
+   if (lightValue <= 150){
+    Serial.println("Plein jour ");
+   }
+   
+   else if(lightValue > 150 & lightValue <= 400){
+    Serial.println("Ciel nuageux ");
+   }
+   else if(lightValue > 400 & lightValue <=700){
+    Serial.println("Ciel obscurci ");
+   }
+
+   else{Serial.println("Nuit noire");}
+
+   delay(1000);
 }
